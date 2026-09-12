ebb2325595738b8e613bd89167b76cf3ee1e0ed1
CodedSarthak <jejurkarsarthak17@gmail.com>
2026-09-12 23:10:13 +0530

    initial setup of project

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.git/**', '**/coverage/**'],
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+);
