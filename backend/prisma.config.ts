21f056b548e1e0ecd0e7dcb6d8fdc030d2933340
CodedSarthak <jejurkarsarthak17@gmail.com>
2026-09-13 13:23:42 +0530

    fixing issues with prisma

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'prisma/config';
 
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
+
 export default defineConfig({
   schema: './prisma/schema.prisma',
 });
